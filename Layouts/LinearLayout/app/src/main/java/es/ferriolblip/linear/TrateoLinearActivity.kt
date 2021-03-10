a1c1785039b251864fe548b248de389b351d4f29
Ferriol-blip <ferriol.zenk@gmail.com>
2021-03-10 10:34:40 +0100

    Trasteo de linear con peso, que es primo hermano de Flex

---
+++ Layouts/LinearLayout/app/src/main/java/es/ferriolblip/linear/TrateoLinearActivity.kt
@@ -0,0 +1,11 @@
+package es.ferriolblip.linear
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TrateoLinearActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trateo_linear)
+    }
+}
