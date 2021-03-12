9778e43a8217240368614ca066010e660a032f9c
Ferriol-blip <ferriol.zenk@gmail.com>
2021-03-12 13:02:31 +0100

    Proyecto Nuevo. importadas librerias del recycleView y Picasso Creada clase dataclass y configurada creado el view (componente) que mostrara y pintara el recycle.

---
+++ RecycleView/MyHeroAcademy/app/src/main/java/es/ferriolblip/myheroacademy/SuperHero.kt
@@ -0,0 +1,8 @@
+package es.ferriolblip.myheroacademy
+//los data class sirver para definir un modelo de datos, se configran las props en el contructor
+data class SuperHero(
+    val superHeroName: String,
+    val realName: String,
+    val publisher: String,
+    val image: String
+)
